99ad0dfcecc8d507e12a1223375013f0791e2821
Tebogo03 <chukwuadrian@gmail.com>
2022-03-08 19:42:03 +0100

    hello

---
+++ src/com/example/helloworld/EvenOdd.java
@@ -0,0 +1,15 @@
+package com.example.helloworld;
+
+public class EvenOdd {
+    public static void main(String[] args) {
+        int randomNumber = (int) (Math.random() *(100 - 1));
+        if (randomNumber%2 ==0){
+            System.out.println(randomNumber +" is an even number");
+        }
+        else {
+            System.out.println(randomNumber +" is an even number");
+
+        }
+    }
+
+}
